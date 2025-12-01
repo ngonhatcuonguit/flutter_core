e90319f23d286e48813026ba9e563a0aabc85e23
cuong.ngo <ngonhatcuonguit@gmail.com>
2025-12-01 11:52:15 +0700

    update project

---
+++ lib/main_dev.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_core_project/core/configs/theme/app_theme.dart';
+import 'package:flutter_core_project/injection_container.dart';
+import 'package:flutter_core_project/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:flutter_core_project/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:flutter_core_project/presentation/pages/splash/splash.dart';
+import 'package:flutter_core_project/services/localization_service.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'presentation/choose_mode/bloc/locale_cubit.dart';
+import 'presentation/choose_mode/bloc/theme_cubit.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load development environment variables
+  await dotenv.load(fileName: ".env.dev");
+
+  // Initialize HydratedBloc storage
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
+  );
+
+  // Initialize the dependency injection
+  await initializeDependencies();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => ThemeCubit()),
+        BlocProvider(create: (_) => LocaleCubit()),
+        BlocProvider(
+            create: (context) =>
+                sl<RemoteArticlesBloc>()..add(const GetArticles()))
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, themeMode) {
+          return BlocBuilder<LocaleCubit, Locale>(
+            builder: (context, locale) {
+              return MaterialApp(
+                title: 'Flutter Core Project (DEV)',
+                theme: AppTheme.lightTheme,
+                darkTheme: AppTheme.darkTheme,
+                themeMode: themeMode,
+                debugShowCheckedModeBanner: true,
+
+                // Localization delegates
+                localizationsDelegates: const [
+                  AppLocalizations.delegate,
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                ],
+                supportedLocales: const [
+                  Locale('en', ''), // English
+                  Locale('vi', ''), // Vietnamese
+                ],
+                locale: locale,
+
+                home: const SplashPage(),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
